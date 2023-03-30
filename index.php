53ba328973be64f9e18b0e8da111c612fb98013b
potatodevs <hoshiku1997@gmail.com>
2023-03-30 10:53:46 +0700

    tailwind-css

--- index.php
+++ index.php
@@ -4,46 +4,49 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Chada Hotel</title>
-    <link rel="stylesheet" href="">
-    <link rel="<link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link href="https://fonts.googleapis.com/css2?family=Merienda&family=Poppins:ital@1&display=swap" rel="stylesheet"><style>
+    <link href="https://fonts.googleapis.com/css2?family=Merienda&family=Poppins:ital@1&display=swap" rel="stylesheet">
+    
+    <style>
     *{
-        font-family: 'Merienda', cursive;
+       
+        font-family: 'Poppins', sans-serif;
     }
     .h-font{
-        font-family: 'Poppins', sans-serif;
+        font-family: 'Merienda', cursive;
     }
 </style>
 </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-light bg-white px-lg-3 py-lg-2 shadow-sm sticky-top">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Chada HOTEL</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <a class="navbar-brand me-5 fw-bold fs-3 h-font" href="index.php">Chada HOTEL</a>
+    <button class="navbar-toggler shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <a class="nav-link active me-2" aria-current="page" href="#">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Rooms</a>
+          <a class="nav-link me-2" href="#">Rooms</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Facilities</a>
+          <a class="nav-link me-2" href="#">Facilities</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Contect Us</a>
+          <a class="nav-link me-2" href="#">Contect Us</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">About</a>
         </li>
       </ul>
       <div class="d-flex">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
+        <button type="button" class="btn btn-outline-dark me-lg-2 me-3" data-bs-toggle="modal" data-bs-target="#loginModel">
+        Login
+        </button>
 </div>
     </div>
   </div>
@@ -52,17 +55,27 @@
 <div class="modal fade" id="loginModel" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
-        <form action="">
+        <form>
         <div class="modal-header">
-        <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        <h5 class="modal-title d-flex align-items-center">
+        <i class="bi bi-person-circle fs-3 me-2"></i> User Login
+        </h5>
+        <button type="button" class="btn-close shadow-none" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Understood</button>
+            <div class="mb-3">
+                 <label class="form-label">Email address</label>
+                 <input type="email" class="form-control shadow-none">
+             </div>
+             <div class="mb-3">
+                 <label class="form-label">Password</label>
+                 <input type="password" class="form-control shadow-none">
+             </div>
+
+             <div>
+                <button type="submit" class="btn btn-dark shadow-none justify-between">LOGIN</button>
+                <a href="javascript:void(1)">Forgot Password</a>
+             </div>
       </div>
         </form>
     </div>
